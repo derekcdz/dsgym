a6872ed678e1ba22d323875d0bfef31554353e4b
derekcdz <derekcodez@gmail.com>
2021-10-09 23:55:28 +0800

    new file: rb_tree, and some parts of implementation

---
+++ tree/rb_tree/rb_tree.go
@@ -0,0 +1,203 @@
+// A map, as known as key-value tables, implemented with red-black tree
+// complexity of inserting, searching and deleting are all O(logN)
+
+package rb_tree
+
+type Key interface {
+	CompareTo(Key) int
+}
+
+type Value interface{}
+
+type Entry struct {
+	Key   Key
+	Value Value
+}
+
+// RBTree should implements SortedMap
+type SortedMap interface {
+	Init()
+	Get(Key) Value
+	Put(Key, Value)
+	Delete(Key)
+	Contains(Key) bool
+	IsEmpty() bool
+	Size() int
+	Min() Key
+	Max() Key
+	Floor(Key) Key
+	Ceiling(Key) Key
+	Select(int) Key
+	Rank(Key) int
+	DeleteMin()
+	DeleteMax()
+	SizeBetween(Key, Key) int
+	Keys() []Key
+	KeysBetween() []Key
+}
+
+const (
+	RED   = true
+	BLACK = false
+)
+
+type color bool
+
+type node struct {
+	color color
+	size  int
+	left  *node
+	right *node
+	key   Key
+	value Value
+}
+
+type RBTree struct {
+	root *node
+	size int
+}
+
+func (x *node) isRed() bool {
+	if x == nil {
+		return false
+	}
+	return x.color == RED
+}
+
+func (x *node) flipColors() {
+	x.color = RED
+	x.left.color = BLACK
+	x.right.color = BLACK
+}
+
+func newNode(k Key, v Value, c color) *node {
+	return &node{
+		color: c,
+		size:  1,
+		left:  nil,
+		right: nil,
+		key:   k,
+		value: v,
+	}
+}
+
+func (x *node) getLeft() *node {
+	if x == nil {
+		return nil
+	}
+	return x.left
+}
+
+func (x *node) getRight() *node {
+	if x == nil {
+		return nil
+	}
+	return x.right
+}
+
+func (x *node) getSize() int {
+	if x == nil {
+		return 0
+	}
+	return x.size
+}
+
+func (x *node) calcSize() {
+	if x == nil {
+		return
+	}
+	x.size = x.left.getSize() + x.right.getSize() + 1
+}
+
+func (x *node) rotateLeft() *node {
+	if x == nil {
+		return nil
+	}
+	res := x.right
+	x.right = res.left
+	res.left = x
+	res.color, x.color = x.color, res.color
+
+	res.calcSize()
+	x.calcSize()
+	return res
+}
+
+func (x *node) rotateRight() *node {
+	if x == nil {
+		return nil
+	}
+	res := x.left
+	x.left = res.right
+	res.right = x
+	res.color, x.color = x.color, res.color
+
+	res.calcSize()
+	x.calcSize()
+	return res
+}
+
+func (x *node) insert(k Key, v Value) *node {
+	if x == nil {
+		return newNode(k, v, RED)
+	} else {
+		cmp := k.CompareTo(x.key)
+		if cmp < 0 {
+			x.left = x.left.insert(k, v)
+		} else if cmp > 0 {
+			x.right = x.right.insert(k, v)
+		} else {
+			x.value = v
+		}
+
+		res := x
+		if x.right.isRed() && !x.left.isRed() {
+			res = res.rotateLeft()
+		}
+		if x.left.isRed() && x.left.getLeft().isRed() {
+			res = res.rotateRight()
+		}
+		if x.left.isRed() && x.right.isRed() {
+			res.flipColors()
+		}
+		res.calcSize()
+		return res
+	}
+}
+
+func (x *node) find(k Key) *node {
+	if x == nil {
+		return nil
+	}
+	cmp := k.CompareTo(x.key)
+	if cmp < 0 {
+		return x.left.find(k)
+	} else if cmp > 0 {
+		return x.right.find(k)
+	} else {
+		return x
+	}
+}
+
+func (t *RBTree) Init() {
+	t.root = nil
+	t.size = 0
+}
+
+func (t *RBTree) Get(k Key) Value {
+	x := t.root.find(k)
+	if x == nil {
+		return nil
+	}
+	return x.value
+}
+
+func (t *RBTree) Put(k Key, v Value) {
+	if t.root == nil {
+		t.root = newNode(k, v, BLACK)
+		t.size = 0
+	} else {
+		t.root = t.root.insert(k, v)
+		t.root.color = BLACK
+	}
+}
--- tree/rbtree_map.go
+++
@@ -1,4 +0,0 @@
-// A map, as known as key-value tables, implemented with red-black tree
-// complexity of inserting, searching and deleting are all O(logN)
-
-package tree
